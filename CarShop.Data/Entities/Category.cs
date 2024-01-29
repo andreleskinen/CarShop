aa0c82bc819c519cf53b39f9048f0f4b8ee7ef83
Josef <josefhemme@hotmail.com>
2024-01-29 10:07:18 +0100

    error fix

--- CarShop.Data/Entities/Category.cs
+++ CarShop.Data/Entities/Category.cs
@@ -3,7 +3,7 @@ namespace CarShop.Data.Entities;
 
 public class Category : IEntity
 {
-    public int CategoryId { get; set; }
+    public int Id { get; set; }
     public string Name { get; set; }
 
     public List<Car> Cars { get; set; }
