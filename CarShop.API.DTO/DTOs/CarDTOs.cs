a0016dcefa1b1546bb5430c06be974174f4da8cd
vonb313 <emil.joakim.berg@hotmail.com>
2024-01-29 09:48:18 +0100

    Error Fix

--- CarShop.API.DTO/DTOs/CarDTOs.cs
+++ CarShop.API.DTO/DTOs/CarDTOs.cs
@@ -1,20 +1,15 @@
 ﻿namespace CarShop.API.DTO;
 
-    public class CarPostDTO
-    {
-        public string Name { get; set; } = string.Empty;
-    }
-    public class CarPutDTO : CarPostDTO
-    {
-        public int Id { get; set; }
-    }
-    public class CarGetDTO : CarPutDTO
-    {
-        //public List<FilterGetDTO>? Filters { get; set; }
-        public List<ProductGetDTO>? Products { get; set; }
-    }
-
-    public class CarSmallGetDTO : CarPutDTO
-    {
-    }
+public class CarPostDTO
+{
+    public string Name { get; set; } = string.Empty;
+}
+public class CarPutDTO : CarPostDTO
+{
+    public int Id { get; set; }
+}
+public class CarGetDTO : CarPutDTO
+{
+    //public List<FilterGetDTO>? Filters { get; set; }
+}
 
--- CarShop.Data/Entities/Category.cs
+++ CarShop.Data/Entities/Category.cs
@@ -3,7 +3,7 @@ namespace CarShop.Data.Entities;
 
 public class Category : IEntity
 {
-    public int CatergoryId { get; set; }
+    public int Id { get; set; }
     public string Name { get; set; }
 
     public List<Car> Cars { get; set; }
